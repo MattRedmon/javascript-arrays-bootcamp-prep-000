e80839b990bb9acb33f505ad9fef64f90bcbd9b9
Matt Redmon <matt.redmon@live.com>
2017-12-12 03:07:34 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array]
+  return newArray
+}
+
+
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = array.unshift(element)
+  return array
+}
+
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return (array.push(element))
+}
+
